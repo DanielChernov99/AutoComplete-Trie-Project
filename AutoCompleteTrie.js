2d9c7dce15cf57492490fc101f353bf6ece849a0
DanielChernov99 <chernov.dani@gmail.com>
2026-06-09 22:19:05 +0300

    Implement addWord method in AutoCompleteTrie class

--- AutoCompleteTrie.js
+++ AutoCompleteTrie.js
@@ -1,7 +1,18 @@
 export default class AutoCompleteTrie{
-    constructor(value,){
+    constructor(value =""){
         this.value = value
         this.children = {}
         this.endOfWord = false
+    }
+    addWord(word){
+        let currentNode = this
+        for(let i = 0; i< word.length; i++ ){
+            let char = word[i]
+            if(!currentNode.children[char]){
+                currentNode.children[char] = new AutoCompleteTrie(char)
+            }
+            currentNode = currentNode.children[char]
+        }
+        currentNode.endOfWord = true
     }
 }
