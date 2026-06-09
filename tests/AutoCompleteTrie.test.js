14b01f6af220307fa81df1e6c38c14e278ddaa80
DanielChernov99 <chernov.dani@gmail.com>
2026-06-09 23:55:41 +0300

    test: add initial test structure for AutoCompleteTrie class

---
+++ tests/AutoCompleteTrie.test.js
@@ -0,0 +1,25 @@
+describe("AutoCompleteTrie", () => {
+    describe("constructor", () => {
+        // tests
+    })
+
+    describe("addWord", () => {
+        // tests
+    })
+
+    describe("findWord", () => {
+        // tests
+    })
+
+    describe("predictWords", () => {
+        // tests
+    })
+
+    describe("_allWordsHelper", () => {
+        // tests
+    })
+
+    describe("_getRemainingTree", () => {
+        // tests
+    })
+})
