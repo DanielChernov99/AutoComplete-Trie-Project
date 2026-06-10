ed5fe1f2b828b55eeb98be4eeb31bace7d245c8f
DanielChernov99 <chernov.dani@gmail.com>
2026-06-10 22:17:47 +0300

    feat: implement ConsoleView class with welcome and help methods

--- src/CLI/ConsoleView.js
+++ src/CLI/ConsoleView.js
@@ -1,3 +1,19 @@
 export default class ConsoleView{
-    
+    constructor(){
+
+    }
+
+    showWelcome(){
+        console.log("=== AutoComplete Trie Console ===")
+        console.log("Type 'help' for commands\n")
+    }
+    showHelp(){   
+        console.log(`Commands:
+                add <word>      - Add word to dictionary
+                find <word>     - Check if word exists
+                complete <prefix> - Get completions
+                help           - Show this message
+                exit           - Quit program  
+        `)
+    }
 }
