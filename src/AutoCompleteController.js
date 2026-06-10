cb0814f44f58b75d4f995951eee1a24873219e78
DanielChernov99 <chernov.dani@gmail.com>
2026-06-10 22:44:26 +0300

    feat: add prompt-sync dependency and enhance AutoCompleteController validation logic

--- src/AutoCompleteController.js
+++ src/AutoCompleteController.js
@@ -1,18 +1,40 @@
 
 export default class AutoCompleteController {
-    constructor(){
-
+    constructor(trie = new AutoCompleteTrie()){
+        this.trie = trie
     }
 
-    handleCommand(args){
+    handleCommand(userInput){
 
     }
-    validateArgs(args){
-        if(!args) return {result: false,message:"please enter command and word"}
-        if(!["add","complete","use","find","help","exit"].include(args[0])){
+    validateArgs(userInput){
+        if(!userInput) return {result: false,message:"please enter command and word"}
+
+        const inputArr = userInput.trim().split(/\s+/)
+
+        if (inputArr.length > 2) return {result: false, message:"Too many arguments"}
+
+        const command = inputArr[0].toLowerCase()
+        const wordCommands = ["add","complete","use","find"]
+        const noWordCommands = ["help","exit"]
+        const allCommands = [...wordCommands, ...noWordCommands]
+
+        if(!(allCommands).includes(command)){
             return {result: false, message:"please enter a valid command"}
         }
+        if(wordCommands.includes(command)){
+            const word = inputArr[1]
+            if (!word) return {result: false, message:"please enter the word aswell"}
+            if (!/^[a-zA-Z]+$/.test(word)) {
+                return { result: false, message: "word must contain only letters" }
+            }
+        }
+        else{
+            const word = inputArr[1]
+            if (word) return {result: false, message:"please try this command without a word"}
+        }       
 
+        return { result: true ,message:"User input is valid"}
     }
 }
 
--- src/CLI/index.js
+++ src/CLI/index.js
@@ -1,14 +1,19 @@
 import AutoCompleteTrie from "../AutoCompleteTrie"
 import AutoCompleteController from "../AutoCompleteController"
 import ConsoleView from "./ConsoleView"
+import promptSync from "prompt-sync"
 
 
 let running = true
 const trieController = new AutoCompleteController()
 const cliView = new ConsoleView()
+const prompt = promptSync()
+
+
+cliView.showWelcome()
 
 while(running){
-
+    let userInput = prompt("> ")
 
 
 
