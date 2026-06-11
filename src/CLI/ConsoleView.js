6971203ab677f6e4932d9acf292d4f7eb22201e2
DanielChernov99 <chernov.dani@gmail.com>
2026-06-11 12:19:08 +0300

    fix: update command list formatting in showHelp method

--- src/CLI/ConsoleView.js
+++ src/CLI/ConsoleView.js
@@ -18,11 +18,12 @@ export default class ConsoleView{
 
     showHelp(){   
         console.log(`Commands:
-add <word>      - Add word to dictionary
-find <word>     - Check if word exists
+add <word>        - Add word to dictionary
+find <word>       - Check if word exists
 complete <prefix> - Get completions
-help           - Show this message
-exit           - Quit program  
+add <word>        - Increase importance of word
+help              - Show this message
+exit              - Quit program  
                     `)
     }
     showResult(commandResult){
