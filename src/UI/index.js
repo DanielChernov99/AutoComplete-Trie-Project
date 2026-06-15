b12275573ddf91a3b62abb94e6120d1908eeebbe
Daniel Chernov <chernov.dani@gmail.com>
2026-06-16 00:02:27 +0300

    feat: add initial HTML and JavaScript files for AutoComplete Trie UI

---
+++ src/UI/index.js
@@ -0,0 +1,23 @@
+import AutoCompleteTrie from "../AutoCompleteTrie.js"
+import AutoCompleteController from "../AutoCompleteController.js"
+import ConsoleView from "./ConsoleView.js"
+import promptSync from "prompt-sync"
+
+
+let running = true
+const trieController = new AutoCompleteController()
+const cliView = new ConsoleView() 
+const prompt = promptSync()
+
+
+cliView.showWelcome()
+
+while(running){
+    let userInput = prompt("> ")
+    const commandResult = trieController.handleCommand(userInput)
+    if(commandResult.commandType ==="exit"){
+        running = false
+    }
+    // cliView.showResult(commandResult)
+    
+}
